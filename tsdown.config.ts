74f3afb949d2659e90fb1b7d2f936dbe6474666c
Vida Xie <vida_2020@163.com>
2025-03-11 20:40:50 +0800

    build: output rules

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,16 @@
 import type { UserConfig } from 'tsdown'
+import { readdirSync, statSync } from 'node:fs'
 import { resolve } from 'node:path'
 import { defineConfig } from 'tsdown'
+
+const RULE_SRC = 'src/rules'
+const rules = readdirSync(RULE_SRC, {
+  withFileTypes: false,
+  recursive: false,
+}).flatMap((dir) => {
+  const name = `${RULE_SRC}/${dir}`
+  return statSync(name).isDirectory() ? [`${name}/${dir}.ts`] : []
+})
 
 export default defineConfig({
   clean: true,
@@ -10,6 +20,7 @@ export default defineConfig({
   },
   entry: [
     'src/index.ts',
+    ...rules,
   ],
   shims: true,
   format: ['cjs', 'esm'],
