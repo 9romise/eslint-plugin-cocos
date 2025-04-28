c5debacb592723da2a442085e00b90ea2c872683
Vida Xie <vida_2020@163.com>
2025-04-28 08:45:43 +0800

    chore: add `match-ccclass-filename` rule to the rules index

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,8 +1,10 @@
 import type { ESLintRuleModule } from '~/utils'
 import ccclassFirst from './ccclass-first/ccclass-first'
 import lifecycleOrder from './lifecycle-order/lifecycle-order'
+import matchCcclassFilename from './match-ccclass-filename/match-ccclass-filename'
 
 export const rules: Record<string, ESLintRuleModule<unknown[], string>> = {
   'ccclass-first': ccclassFirst,
   'lifecycle-order': lifecycleOrder,
+  'match-ccclass-filename': matchCcclassFilename,
 }
